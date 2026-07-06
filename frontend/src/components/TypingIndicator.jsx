a74075dd6431a9ecd85e647bd112c6518fa2fae8
24pransh-creator <24pransh@gmail.com>
2026-07-06 18:04:58 +0530

    Add typing indicator component

---
+++ frontend/src/components/TypingIndicator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TypingIndicator() {
+  return (
+    <div
+      style={{
+        padding: "15px",
+        color: "#ccc",
+        fontStyle: "italic"
+      }}
+    >
+      🤖 Pransh AI is typing...
+    </div>
+  );
+}
